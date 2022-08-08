3dad94ba4d0140b60c3e486fa1f035c7cb060663
alexrocha97 <venturarocha08@gmail.com>
2022-08-07 23:33:49 -0300

    Inicio da implementação da controller da noticia

--- API/Controllers/loginController.cs
+++ API/Controllers/loginController.cs
@@ -27,50 +27,52 @@ namespace API.Controllers
             _appUsuario = appUsuario;
         }
 
-        [HttpPost("/api/CreateToken")]
-        [AllowAnonymous]
-        [Produces("application/json")]
-        public async Task<IActionResult> CreateToken([FromBody]Login login)
-        {
-            if(string.IsNullOrWhiteSpace(login.email) || string.IsNullOrWhiteSpace(login.senha))
-            {
-                return Unauthorized();
-            }
-            var result = await _appUsuario.IsExistsUser(login.email, login.senha);
-            if(result)
-            {
-                var token = new JsonWebTokenBuild()
-                    .AddSecurityKey(JsonWebTokenSecurity
-                        .Create("Secret_Key-12345678"))
-                        .AddSubject("Empresa - Canal de Noticia")
-                        .AddIssuer("Teste.Securiry.Bearer")
-                        .AddAudience("Teste.Securiry.Bearer").AddClaim("UsuarioAPINumero", "1")
-                        .AddExpiry(5)
-                        .Builder();
+        // [HttpPost("/api/CreateToken")]
+        // [AllowAnonymous]
+        // [Produces("application/json")]
+        // public async Task<IActionResult> CreateToken([FromBody]Login login)
+        // {
+        //     if(string.IsNullOrWhiteSpace(login.email) || string.IsNullOrWhiteSpace(login.senha))
+        //     {
+        //         return Unauthorized();
+        //     }
+        //     var result = await _appUsuario.IsExistsUser(login.email, login.senha);
+        //     if(result)
+        //     {
+                // var IdUsuario = await _appUsuario.RetornoIdUsuario(login.email);
+
+        //         var token = new JsonWebTokenBuild()
+        //             .AddSecurityKey(JsonWebTokenSecurity
+        //                 .Create("Secret_Key-12345678"))
+        //                 .AddSubject("Empresa - Canal de Noticia")
+        //                 .AddIssuer("Teste.Securiry.Bearer")
+        //                 .AddAudience("Teste.Securiry.Bearer").AddClaim("IdUsuario", IdUsuario)
+        //                 .AddExpiry(5)
+        //                 .Builder();
                 
-                return Ok(token.value);
-            }
-            else
-            {
-                return Unauthorized();
-            }
-        }
-        [HttpPost("/api/CreateUsuario")]
-        [AllowAnonymous]
-        [Produces("application/json")]
-        public async Task<IActionResult> CreateUsuario([FromBody]Login login)
-        {
-            if(string.IsNullOrWhiteSpace(login.email) || string.IsNullOrWhiteSpace(login.senha))
-            {
-                return Unauthorized();
-            }
-            var result = await 
-                _appUsuario.AddUsuario(login.email, login.senha,login.idade,login.celular);
-            if(result)
-                return Ok("Criado o usuário com sucesso!");
-            else
-                return BadRequest("Erro ao criar usuário");
-        }
+        //         return Ok(token.value);
+        //     }
+        //     else
+        //     {
+        //         return Unauthorized();
+        //     }
+        // }
+        // [HttpPost("/api/CreateUsuario")]
+        // [AllowAnonymous]
+        // [Produces("application/json")]
+        // public async Task<IActionResult> CreateUsuario([FromBody]Login login)
+        // {
+        //     if(string.IsNullOrWhiteSpace(login.email) || string.IsNullOrWhiteSpace(login.senha))
+        //     {
+        //         return Unauthorized();
+        //     }
+        //     var result = await 
+        //         _appUsuario.AddUsuario(login.email, login.senha,login.idade,login.celular);
+        //     if(result)
+        //         return Ok("Criado o usuário com sucesso!");
+        //     else
+        //         return BadRequest("Erro ao criar usuário");
+        // }
 
         // MESMA FORMA  DO ENDPOINT CREATETOKEN, MAS USANDO AS FERRAMENTAS DO IDENTITY
         [HttpPost("/api/CreateTokenIdentity")]
@@ -87,12 +89,15 @@ namespace API.Controllers
             
             if(result.Succeeded)
             {
+                var IdUsuario = await _appUsuario.RetornoIdUsuario(login.email);
+
                 var token = new JsonWebTokenBuild()
                     .AddSecurityKey(JsonWebTokenSecurity
                         .Create("Secret_Key-12345678"))
                         .AddSubject("Empresa - Canal de Noticia")
                         .AddIssuer("Teste.Securiry.Bearer")
-                        .AddAudience("Teste.Securiry.Bearer").AddClaim("UsuarioAPINumero", "1")
+                        .AddAudience("Teste.Securiry.Bearer")
+                        .AddClaim("IdUsuario", IdUsuario)
                         .AddExpiry(5)
                         .Builder();
                 
@@ -142,5 +147,7 @@ namespace API.Controllers
             return BadRequest("Erro ao confirmar usuário.");
 
         }
+
+
     }
 }
---
+++ API/ViewModels/NoticiaMViewModel.cs
@@ -0,0 +1,10 @@
+namespace API.ViewModels
+{
+    public class NoticiaMViewModel
+    {
+        public int IdNoticia {get; set;}
+        public string Titulo {get; set;}
+        public string Informacao {get; set;}
+        public int IdUsuario {get; set;}
+    }
+}
